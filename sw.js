d0aa470126a85496570fe2c82f76b32091db5384
silovar-uk <46244124+silovar-uk@users.noreply.github.com>
2026-07-13 22:41:17 +0900

    Cache the visual guide carousel

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "zelda-lab-v1";
-const ASSETS = ["./", "./index.html", "./styles.css", "./data.js", "./app.js", "./icon.svg", "./manifest.webmanifest"];
+const CACHE = "zelda-lab-v2";
+const ASSETS = ["./", "./index.html", "./styles.css", "./guides.css", "./data.js", "./app.js", "./guides.js", "./icon.svg", "./manifest.webmanifest"];
 self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS))));
 self.addEventListener("activate", (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key))))));
 self.addEventListener("fetch", (event) => {
